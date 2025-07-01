c6caf3b63335930defcc9390a7b1871e0195a705
PrateekP <prateekpanta1789@gmail.com>
2025-07-01 10:06:55 +0530

    Added an index file that exports all the images

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import danielImage from './image-daniel.jpg'
+import jonathanImage from './image-jonathan.jpg'
+import kiraImage from './image-kira.jpg'
+import jeanetteImage from './image-jeanette.jpg'
+import patrickImage from './image-patrick.jpg'
+import bgQuote from './bg-pattern-quotation.svg'
+
+export const images = {
+    danielImage,
+    jonathanImage,
+    kiraImage,
+    jeanetteImage,
+    patrickImage,
+    bgQuote,
+}
